019b750d43eb17c2b7073ee1853c85894fdeb2be
Joff Tiquez <jofftiquez@gmail.com>
2018-05-14 13:23:14 +0800

    Update CSS

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import component from './components';
 
 const link = document.createElement('link');
 link.rel = 'stylesheet';
-link.href = 'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.2.1/cropper.min.css';
+link.href = 'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.3.5/cropper.min.css';
 document.getElementsByTagName('head')[0].appendChild(link);
 
 const CropperJsVue = {
